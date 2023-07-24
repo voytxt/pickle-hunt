991cef7399bbe436a2a0be243256a8eb13ca33eb
Vojta <65811166+vojta-dev@users.noreply.github.com>
2023-07-24 23:36:37 +0200

    Add Vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,11 @@
   import '@skeletonlabs/skeleton/styles/skeleton.css';
 
   import '../app.postcss';
+
+  import { dev } from '$app/environment';
+  import { inject } from '@vercel/analytics';
+
+  inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <slot />
