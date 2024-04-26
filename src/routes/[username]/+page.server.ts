f2ed8d63e40190651d392a08703b5f707b3c0754
Vojta <65811166+voytxt@users.noreply.github.com>
2024-04-26 15:22:24 +0200

    upgrade everything

--- src/routes/[username]/+page.server.ts
+++ src/routes/[username]/+page.server.ts
@@ -32,17 +32,17 @@ async function getUuid(username: string): Promise<string> {
     .get(`https://api.mojang.com/users/profiles/minecraft/${username}`)
     .badRequest((err) => {
       console.error(`< [400] ${err.json.error}: ${err.json.errorMessage}`);
-      throw error(400, err.json.errorMessage);
+      error(400, err.json.errorMessage);
     })
     .notFound((err) => {
       console.error(`< [404] ${err.json.errorMessage}`);
-      throw error(404, err.json.errorMessage);
+      error(404, err.json.errorMessage);
     })
     .json();
 
   if (response.id === undefined) {
     console.error(`< [500] ${JSON.stringify(response)}`);
-    throw error(500, JSON.stringify(response));
+    error(500, JSON.stringify(response));
   }
 
   return response.id;
@@ -67,15 +67,15 @@ async function getStats(uuid: string): Promise<{
     .get(`https://api.hypixel.net/player?key=${HYPIXEL_API_KEY}&uuid=${uuid}`)
     .badRequest((err) => {
       console.error(`< [400] ${err.json.cause}`);
-      throw error(400, err.json.cause);
+      error(400, err.json.cause);
     })
     .forbidden((err) => {
       console.error(`< [403] ${err.json.cause}`);
-      throw error(403, err.json.cause);
+      error(403, err.json.cause);
     })
     .error(429, (err) => {
       console.error(`< [429] ${err.json.cause}`);
-      throw error(429, err.json.cause);
+      error(429, err.json.cause);
     })
     .json();
 
@@ -83,7 +83,7 @@ async function getStats(uuid: string): Promise<{
   // if a player joined before APs were a thing, achievementPoints can be undefined (936555d35b624d74a6b952db395756c1, notch)
   if (!response.success || response.player == null || response.player.achievementPoints === undefined) {
     console.error(`< [500] ${JSON.stringify(response)}`);
-    throw error(500, JSON.stringify(response));
+    error(500, JSON.stringify(response));
   }
 
   return {
